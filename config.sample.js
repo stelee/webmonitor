07f5e52ce3d9c155394ae327f09602b8e74ca185
unknown <yli@MSC-R96RKT6.allegisgroup.com>
2014-04-02 17:16:04 -0400

    add sample config file

---
+++ config.sample.js
@@ -0,0 +1,12 @@
+//only support hotmail, you can change the service inside mailer.js if you like
+exports.config={
+	urls:[
+		"https://www.google.com",
+		"http://www.google.com"
+	],
+	mail:{
+		username:"yourname@yourdomain",
+		password:"xxxx"
+	},
+	strict:true
+}
